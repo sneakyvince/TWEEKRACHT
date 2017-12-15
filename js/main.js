4c3579100883b6b3c4eacf0b7d8a91a3bad83e67
Vincent van der Palen <vincent@MacBook-Pro-van-Vincent.local>
2017-12-15 10:02:51 +0100

    adds project

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$('.door').click(function(){
+    $('.door').toggleClass('doorOpen');
+  });
+
+  $('.door2').click(function(){
+    $('.door2').toggleClass('doorOpen');
+  });
+
+
+
+  function leftArrowPressed() {
+    $('.door').toggleClass('doorOpen');
+
+    setTimeout(function(){
+        window.location.href ='dromen.html';
+    }, 2000);
+    
+ }
+ 
+ function rightArrowPressed() {
+    $('.door2').toggleClass('doorOpen');
+    
+    setTimeout(function(){
+        window.location.href ='durven.html';
+    }, 2000);
+ }
+ 
+ document.onkeydown = function(evt) {
+     evt = evt || window.event;
+     switch (evt.keyCode) {
+         case 37:
+             leftArrowPressed();
+             break;
+         case 39:
+             rightArrowPressed();
+             break;
+     }
+ };
