c5bb280b8e915c37b16da27eb7cb264f0d7621b2
Vincent van der Palen <vincent@MacBook-Pro-van-Vincent.local>
2017-12-15 14:01:59 +0100

    adds some test pages for “dromen” cards
    These cards are a test of how the navigation works with the javascript func.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-$('.door').click(function(){
+$('.door').click(function () {
     $('.door').toggleClass('doorOpen');
-  });
+});
 
-  $('.door2').click(function(){
+$('.door2').click(function () {
     $('.door2').toggleClass('doorOpen');
-  });
+});
 
 
-
-  function leftArrowPressed() {
+function leftArrowPressed() {
     $('.door').toggleClass('doorOpen');
 
-    setTimeout(function(){
-        window.location.href ='dromen.html';
+    setTimeout(function () {
+        window.location.href = $('.pageDirection.left').attr('id') + '.html';
     }, 2000);
-    
- }
- 
- function rightArrowPressed() {
+
+}
+
+function rightArrowPressed() {
     $('.door2').toggleClass('doorOpen');
-    
-    setTimeout(function(){
-        window.location.href ='durven.html';
+
+    setTimeout(function () {
+        window.location.href = $('.pageDirection.right').attr('id') + '.html';
     }, 2000);
- }
- 
- document.onkeydown = function(evt) {
-     evt = evt || window.event;
-     switch (evt.keyCode) {
-         case 37:
-             leftArrowPressed();
-             break;
-         case 39:
-             rightArrowPressed();
-             break;
-     }
- };
+}
+
+document.onkeydown = function (evt) {
+
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+    }
+};
