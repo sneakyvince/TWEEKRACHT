1f1b33ecba014a2fc3a28935d8dbe912caebc445
Vincent van der Palen <vincent@MacBook-Pro-van-Vincent.local>
2018-01-11 13:54:11 +0100

    adds 3 doors

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ $('.door2').click(function () {
     $('.door2').toggleClass('doorOpen');
 });
 
+$('.door3').click(function () {
+    $('.door3').toggleClass('doorOpen');
+});
 
 function leftArrowPressed() {
     $('.door').toggleClass('doorOpen');
@@ -24,6 +27,14 @@ function rightArrowPressed() {
     }, 2000);
 }
 
+function upArrowPressed() {
+    $('.door3').toggleClass('doorOpen');
+
+    setTimeout(function () {
+        window.location.href = $('.pageDirection.up').attr('id') + '.html';
+    }, 2000);
+}
+
 document.onkeydown = function (evt) {
 
     evt = evt || window.event;
@@ -33,6 +44,9 @@ document.onkeydown = function (evt) {
             break;
         case 39:
             rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
             break;
     }
 };
